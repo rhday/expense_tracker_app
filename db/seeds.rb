a84e514857ad224bb998cf5b882e89d99e0fea02
rhday <54642787+rhday@users.noreply.github.com>
2020-09-04 13:43:50 +0100

    issue with seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 user_account = UserAccount.create(name: 'Current Account', balance: 1000)
 
-transaction = Transaction.create(account_id: 1, category_id: 1, amount: 10, kind: 'deposit', date:Date.today)
+transaction = Transaction.create(account_id: 1, category_id: 1, amount: 10.00, kind: 'deposit', date: Date.today)
 
-category = Category.create(name: 'Found Money')
+transaction_two = Transaction.create(account_id: 1, category_id: 1, amount: 10.00, kind: 'withdraw', date: Date.today)
+
+category = Category.create(name: 'Money')
