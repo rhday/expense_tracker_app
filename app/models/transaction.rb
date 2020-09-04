9daa216ad93976d7a1054e9bb1b18978a4f14372
rhday <54642787+rhday@users.noreply.github.com>
2020-09-04 13:21:06 +0100

    seeded my database

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,6 @@ class Transaction < ApplicationRecord
     belongs_to :user_account
     belongs_to :category
     validates :amount, presence: true
+    #below means that a ':kind' of value must be included. The user must specify whether it is a deposit or a withdrawal.
+    validates_inclusion_of :kind, :in => ['deposit', 'withdraw']
 end
