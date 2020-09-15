ff3b9c3b5a206815397c84085c79ea3d313b70ff
rhday <54642787+rhday@users.noreply.github.com>
2020-09-15 13:18:34 +0100

    user_controller updated

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -6,15 +6,22 @@ class UserAccountsController < ApplicationController
     end 
 
     def create
-
+        @user = User.new(user_params)
+        if @account.save
+            render json: @user
+        else 
+            render json: {error: "Error creating User account"}
+        end
     end
 
     def show
-
+        @user = User.find(params[:id])
+        render json: @user
     end
 
     def destroy 
-
+        @user = User.find(params[:id])
+        @user.destroy
     end 
 
     private 
