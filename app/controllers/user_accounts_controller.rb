ff1c01e699a7715b8601c72b68a1cb01c420c5ba
rhday <54642787+rhday@users.noreply.github.com>
2020-09-15 13:11:42 +0100

    started to build out controllers

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -1,2 +1,26 @@
 class UserAccountsController < ApplicationController
+
+    def index
+        @users = User.all 
+        render json: @users
+    end 
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy 
+
+    end 
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :balance)
+    end 
+
 end
