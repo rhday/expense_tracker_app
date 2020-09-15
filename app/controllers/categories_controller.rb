e7552adddfbcd2504ea6c434c9a48b62a8c3c0e8
rhday <54642787+rhday@users.noreply.github.com>
2020-09-15 13:37:10 +0100

    started building transactions_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,25 @@
 class CategoriesController < ApplicationController
+
+    def index
+      
+    end 
+
+    def create
+        
+    end
+
+    def show
+        
+    end
+
+    def destroy 
+        
+    end 
+
+    private 
+
+    def user_params
+        params.require(:transaction).permit(:user_id, :category_id, :amount, :kind, :date)
+    end 
+    
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,32 @@
 class TransactionsController < ApplicationController
+
+    before_action :set_user
+
+    def index
+      @transactions = @user.transactions
+      render json: @transactions
+    end 
+
+    def show
+       
+    end
+
+    def create
+        
+    end
+
+    def destroy 
+        
+    end 
+
+    private 
+
+    def set_user
+        @user = User.find(params[:user_id])
+    end 
+
+    def user_params
+        params.require(:transaction).permit(:user_id, :category_id, :amount, :kind, :date)
+    end 
+
 end
